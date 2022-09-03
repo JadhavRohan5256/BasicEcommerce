1cb885029d3fbdc2759e7461b9b5c72d789ea622
Rohan Ramhari Jadhav <91401940+JadhavRohan5256@users.noreply.github.com>
2022-09-03 14:17:11 +0530

    Update checkout.js

--- BasicEcommerce/src/main/webapp/js/checkout.js
+++ BasicEcommerce/src/main/webapp/js/checkout.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 				if(response.status == "created") {
 					saveOrderDatabase();
 					let options={
-						key:"rzp_test_DIhp8Aqjojcgzl",
+						key:"",  // rozar pay api - key
 						amount:response.amount,
 						currency:response.currency,
 						image:"https://images.unsplash.com/photo-1653202916210-9e41f0173b6b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070",
@@ -98,7 +98,6 @@ function updateOrders() {
 			}
 		}
 	});
-}
 
 
 // save order to database
